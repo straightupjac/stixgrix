1b5c9271ae6a8028f63f11db1d1c0ee2fd5e043c
straightupjac <16831223+straightupjac@users.noreply.github.com>
2021-02-01 12:42:02 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@
 set -e
 
 # build
-npm run build
-
-# navigate into the build output directory
-cd dist
+yarn build
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
+git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:straightupjac/stixgrix.git master:gh-pages
+# git push -f git@github.com:straightupjac/stixgrix.git master:gh-pages
+
+git push
 
 cd -
