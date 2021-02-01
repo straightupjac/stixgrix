13d32930ebbfb3d67c750069b25fae2e81673944
straightupjac <16831223+straightupjac@users.noreply.github.com>
2021-02-01 11:48:01 +0800

    clean up

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/stixgrix/'
     : '/',
+    outputDir: 'docs',
+    filenameHashing: false,
+    productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
   ]
